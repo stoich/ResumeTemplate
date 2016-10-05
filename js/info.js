e116dc9ce04ae2068966e50f9aaf0a2887b45d57
Stoil Stoichev <stoilstoychev@gmail.com>
2016-10-05 01:13:38 +0100

    initial commit

---
+++ js/info.js
@@ -0,0 +1,75 @@
+var resume = {
+    mainInfo: {
+        name: "John Smith",
+        profilePicUrl: "http://slashstars.com/wp-content/uploads/2015/08/StoilTeamMemberPicture.jpg",
+        title: "Software Engineer",
+        about: "I'm an IT professional with around 7 years of experience. I come from a QA background and my main interest is web applications and the everchanging technologies that power them. I'm a skilled test automation developer with experience in the design and implementation of test frameworks based around a continuous integration process. In my spare time I program games with the Unity Game Engine",
+        skills: [
+            {title: "Programming languages:", description: "JavaScript, C#"},
+            {title: "Version control:", description: "SVN, GIT, TFS",},
+            {
+                title: "Frameworks and servers:",
+                description: "Bootstrap, jQuery, Kendo UI, AngularJS, NodeJS, Selenium, Protractor, Jasmine, Telerik Testing Framework, ASP .Net MVC"
+            },
+            {title: "Others:", description: "Unity Game Engine"}
+        ],
+        languages: " English, Sokovian(native)",
+        contact: {
+            email: "abc@user.com",
+            phone: "+359121212121",
+            linkedin: "https://uk.linkedin.com/in/stoil-stoichev-9008aa93",
+            github: "https://github.com/stoich/"
+        }
+    },
+    education: [
+        {
+            timeRange: "2007-2011",
+            title: " Bachelor's degree, Computer science",
+            description: "Technical University of Sokovia"
+        },
+        {
+            timeRange: "2002-2007",
+            title: "High School degree",
+            description: "Sokovia Math Highschool"
+        }
+    ],
+    experience: [
+        {
+            timeRange: "2012-2016",
+            title: "Front End Dev at Super Company Inc.",
+            description: "I work with css and stuff. It's OK.",
+            technology: "JavaScript, jQuery, CSS"
+        },
+        {
+            timeRange: "2010-2012",
+            title: "Java Dev at Cubicle Servers Inc.",
+            description: "I work with Eclipse. It's enjoyable. Once I put a joke in the comments",
+            technology: "Java EE, Eclipse, MyDB"
+        }
+    ],
+    projects: [
+        {
+            timeRange: "April - May 2016",
+            title: "Develop a wiki bot",
+            description: "I worked on making a bot in GoLang",
+            technology: "GoLang, Notepad"
+        },
+        {
+            timeRange: "Feb 2015",
+            title: "Develop an api for a thing",
+            description: "We made the api, wasn't easy.",
+            technology: "Java, MySQL"
+        }
+    ],
+    others: [
+
+        {
+            title: "Teaching kids",
+            description: "I worked with kids and stuff."
+        },
+        {
+            title: "Teaching pets",
+            description: "I taught my cat to write C# code."
+        }
+    ]
+};
---
+++ js/main.js
@@ -0,0 +1,4 @@
+angular.module('ngRepeat', ['ngAnimate']).controller('repeatController', function ($scope) {
+    $scope.friends = info;
+    $scope.mainInfo = resume.mainInfo;
+});
